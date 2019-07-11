3fcdec70b804cd64eed84c9d192df0c3e57bb601
samrat.basu01 <samrat.basu01@10.0.75.1>
2019-07-11 11:22:46 +0530

    uPDATED

--- src/main/java/com/infy/cpc/fas/dao/CPCFasDao.java
+++ src/main/java/com/infy/cpc/fas/dao/CPCFasDao.java
@@ -1,26 +1,18 @@
 package com.infy.cpc.fas.dao;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+
 import org.apache.log4j.Logger;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.infy.cpc.fas.entity.Address;
 import com.infy.cpc.fas.entity.User;
@@ -30,6 +22,7 @@ import com.infy.cpc.fas.repository.CRUDUserRepository;
 import com.infy.cpc.fas.request.FASRequestAddress;
 import com.infy.cpc.fas.request.FASRequestBody;
 import com.infy.cpc.fas.response.ResponseStatus;
+
 
 @Repository
 public class CPCFasDao<T> {
@@ -73,8 +66,32 @@ public class CPCFasDao<T> {
 		return response;
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<?> getStudentDataSet(String id) {
-		List<StudentDetails> list = em.createNamedQuery("GET_STUDENT_DETAILS").setParameter("id", Integer.parseInt(id)).getResultList();
+		
+		List<StudentDetails> list = new ArrayList<>();
+		list = em.createNamedQuery("GET_STUDENT_DETAILS_HQL_QUERY").setParameter("id", Integer.parseInt(id)).getResultList();
+		ObjectMapper mapper = new ObjectMapper();
+		int index =0;
+		for (Object sd : list) {
+			try {
+				String jsonInString=mapper.writeValueAsString(list.get(index)).toString();
+				Map<String, Object> map=mapper.readValue(jsonInString, Map.class);
+				log.debug("iterated result :"+map.get("address"));
+				index++;
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		/* Without OBJECT through JPQL Working*/
+		/*List<Object[]> list = new ArrayList<>();
+		list = em.createNamedQuery("GET_STUDENT_DETAILS_NATIVE_QUERY").setParameter("id", Integer.parseInt(id)).getResultList();
+		for (Object[] sd : list)
+			log.debug("iterated result - name :"+sd[1]+" DOB :"+sd[2]+" address :"+sd[4]+" and id :"+sd[0]);*/
+		
+		
 		if (!list.isEmpty())
 			return list;
 		else {
